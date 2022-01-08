a8daa2383e6605fcd6a9e09906b00ad8c08a2b95
Luis Fernando <nando.lfp@hotmail.com>
2022-01-08 19:37:56 -0300

    arrow functions

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// A R R O W  F U N C T I O N S
+
+/*
+function Car() {
+    this.Cor = 'Azul'
+}
+console.log(new Car());
+*/
+
+/*
+var sumOld = function(a, b) {
+    return a + b;
+};
+
+var sum = (a, b) => a + b
+console.log(sum(5, 15));
+*/
+
+/*
+var sum = function(a, b) {
+    return a + b;
+};
+console.log(sum(5,5));
+*/
+
+/*
+function log(value){
+    console.log(value);
+}
+log('test')
+*/
